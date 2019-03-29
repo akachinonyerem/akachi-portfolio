d2b084e48b0edf77d5cf5402c8105ffd8c441c4e
Akachi Ogbonna <akachinonyerem.o@gamail.com>
2019-03-29 21:55:37 +0100

    upgraded version lol

--- js/script.js
+++ js/script.js
@@ -29,23 +29,25 @@ document.getElementById('mobile-nav-trigger').addEventListener('click', function
 var homeBg = $(".home-bg");
 var titleText = $(".header-title-text");
 var bodyTxt = $(".header-text");
-var homeBtn = $(".header-btn");
+var homeBtn = $(".header-btn-wrapper");
 var homeTl = new TimelineMax();
 
+TweenMax.from(".navbar", 0.5, {y: -5, opacity: 0, autoalpha: 0, ease:Power1.easeOut, delay: 0.5})
 homeTl 
-.from(homeBg, 1, {opacity: 0, autoalpha: 0, ease:Power1.easeOut, delay: 0.8})
-.from(titleText, 1, {y: 800, opacity: 0, autoalpha: 0, ease:Power3.easeOut})
-.from(bodyTxt, 1, {y:500, opacity: 0, autoalpha: 0, ease:Power4.easeOut})
-.from(homeBtn, 1, {y: 500, opacity: 0, autoalpha: 0, ease:Power1.easeOut});
+.from(homeBg, 0.8, {opacity: 0, autoalpha: 0, ease:Power1.easeOut, delay: 1})
+.from(titleText, 0.8, {y: 50, opacity: 0, autoalpha: 0, ease:Power3.easeOut})
+.from(bodyTxt, 0.8, {y:50, opacity: 0, autoalpha: 0, ease:Power4.easeOut})
+.from(homeBtn, 0.6, {y: 30, opacity: 0, autoalpha: 0, ease:Power1.easeOut});
 
 
-TweenMax.from(".process-bg", 1, {opacity: 0, autoalpha: 0, ease:Power1.easeOut, delay: 0.8})
-TweenMax.from(".work-item-frame", 1, {opacity: 0, autoalpha: 0, ease:Power1.easeInOut, delay: 4});
-TweenMax.from(".contact-item", 1, {opacity: 0, autoalpha: 0, ease:Power1.easeInOut, delay: 4});
+TweenMax.from(".process-bg", 0.8, {opacity: 0, autoalpha: 0, ease:Power1.easeOut, delay: 1});
 
-TweenMax.from(".project-title", 1, {x: -800, opacity: 0, autoalpha: 0, ease:Power3.easeOut});
-TweenMax.from(".work-type-box", 1, {x: 800, opacity: 0, autoalpha: 0, ease:Power3.easeInOut , delay: 1});
-TweenMax.from(".work-type", 1, {x: -800, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 2});
-TweenMax.from(".project-desc-box", 1, {y: 200, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 3});
-TweenMax.from(".logo-process", 1, {y: 200, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 3});
-TweenMax.from(".brand-work-section", 1, {y: 300, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 4.5});
+TweenMax.from(".work-contact-main-con", 0.8, {opacity: 0, autoalpha: 0, ease:Power2.easeInOut, delay: 3.2});
+
+
+TweenMax.from(".project-title", 0.8, {x: -80, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 0.8});
+TweenMax.from(".work-type-box", 0.8, {x: 80, opacity: 0, autoalpha: 0, ease:Power3.easeInOut, delay: 1.2});
+TweenMax.from(".work-type", 0.8, {x: -80, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 2.2});
+TweenMax.from(".project-desc-box", 0.8, {y: 20, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 3.2});
+TweenMax.from(".logo-process", 0.8, {y: 20, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 3.2});
+TweenMax.from(".brand-work-section", 0.8, {y: 30, opacity: 0, autoalpha: 0, ease:Power3.easeOut, delay: 4});
